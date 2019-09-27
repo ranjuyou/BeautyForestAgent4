c9a529b645d1587784ecac31fd8e1760f976a6a5
YouRanju <dbfkswn0321@naver.com>
2019-09-27 11:49:41 +0900

    #3 코드구현 3211 유란주

--- BeautyForestAgent/FormMemo.Designer.cs
+++ BeautyForestAgent/FormMemo.Designer.cs
@@ -140,6 +140,7 @@
             this.btnLoadFileSelect.TabIndex = 2;
             this.btnLoadFileSelect.Text = "파일 선택";
             this.btnLoadFileSelect.UseVisualStyleBackColor = true;
+            this.btnLoadFileSelect.Click += new System.EventHandler(this.BtnLoadFileSelect_Click);
             // 
             // btnLoadFile
             // 
@@ -149,6 +150,7 @@
             this.btnLoadFile.TabIndex = 3;
             this.btnLoadFile.Text = "불러오기";
             this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.BtnLoadFile_Click);
             // 
             // btnSaveFile
             // 
@@ -158,6 +160,7 @@
             this.btnSaveFile.TabIndex = 7;
             this.btnSaveFile.Text = "저장하기";
             this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.BtnSaveFile_Click);
             // 
             // btnSaveFileSelect
             // 
@@ -167,6 +170,7 @@
             this.btnSaveFileSelect.TabIndex = 6;
             this.btnSaveFileSelect.Text = "파일 선택";
             this.btnSaveFileSelect.UseVisualStyleBackColor = true;
+            this.btnSaveFileSelect.Click += new System.EventHandler(this.BtnSaveFileSelect_Click);
             // 
             // txtSaveText
             // 
